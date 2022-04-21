6d5429f8c8a637879b4219d3a2a2b8d831e5ba77
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-04-21 15:19:55 -0400

    Design HashSet

---
+++ design-hashset/sol.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/design-hashset/
+
+var MyHashSet = function () {
+	this.hashSet = {}
+};
+
+MyHashSet.prototype.add = function (key) {
+	this.hashSet[key] = key
+};
+
+MyHashSet.prototype.remove = function (key) {
+	delete this.hashSet[key]
+};
+
+MyHashSet.prototype.contains = function (key) {
+	return key in this.hashSet
+};
